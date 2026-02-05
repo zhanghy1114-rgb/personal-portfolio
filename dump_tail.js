7e7b4a004c03259c000d652d9187014104216b52
zhanghy1114-rgb <zhanghy1114@gmail.com>
2026-02-05 20:48:39 +0800

    Fix: Restore missing icons for tools (WaytoAGI, Coze, Miaoda)

--- dump_tail.js
+++
@@ -1,7 +0,0 @@
-
-const fs = require('fs');
-const path = require('path');
-const dbPath = path.join(__dirname, 'data', 'db.json');
-const data = fs.readFileSync(dbPath, 'utf8');
-const len = data.length;
-console.log(JSON.stringify(data.substring(len - 200, len)));
